f1820f3c01c9abba44865235016e6d4bda6c4f40
avfisher <brianwangrewa@gmail.com>
2015-05-11 17:19:30 +0800

    Update checkVT.py

--- checkVT.py
+++ checkVT.py
@@ -2,8 +2,8 @@
 # -*- coding:utf8 -*-
 
 # Description: check Hashes in VirusTotal.com
-# Author: avfisher
-# Date: 2015.05.11
+# Author: renfei
+# Date: 2015.05.08
 
 import urllib2
 import re
@@ -21,6 +21,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
+from email.mime.text import MIMEText
 from bs4 import BeautifulSoup
 
 def sha256(filename):
@@ -28,6 +29,8 @@ def sha256(filename):
     sh = hashlib.sha256()
     sh.update(f.read())
     fhash = sh.hexdigest()+","+filename
+    now = time.strftime('%H:%M:%S',time.localtime(time.time()))
+    print str(now)+": "+sh.hexdigest()
     f.close()
     return fhash
 
@@ -43,16 +46,16 @@ def get_all_file(path):
 
 def get_file_hash(path):
 
-    #f = open('hash_sha256.txt','w')
+    f = open('hash_sha256.txt','w')
     hashlist=[]
     list1=get_all_file(path)
-    #print list1
     for i in range(0,len(list1)):
         if (os.path.isfile(list1[i]) == True ):
             fhash = sha256(list1[i])
             hashlist.append(fhash)
-            #f.write(fhash+'\n')
-    #f.close
+            #print fhash
+            f.write(fhash+'\n')
+    f.close
     return hashlist
 
 def errorlog(log):
@@ -62,30 +65,29 @@ def errorlog(log):
     f.close
 
 def getUrlRespHtml(url):
-    heads = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
-             'Accept-Charset':'GB2312,utf-8;q=0.7,*;q=0.7', 
-             'Accept-Language':'zh-cn,zh;q=0.5', 
-             'Cache-Control':'max-age=0', 
-             'Connection':'keep-alive', 
-             'Keep-Alive':'115',
-             'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.14) Gecko/20110221 Ubuntu/10.10 (maverick) Firefox/3.6.14'}
- 
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-    urllib2.install_opener(opener) 
-    req = urllib2.Request(url)
-    opener.addheaders = heads.items()
-    respHtml = opener.open(req).read()
+    try:
+        heads = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
+                 'Accept-Charset':'GB2312,utf-8;q=0.7,*;q=0.7', 
+                 'Accept-Language':'zh-cn,zh;q=0.5', 
+                 'Cache-Control':'max-age=0', 
+                 'Connection':'keep-alive', 
+                 'Keep-Alive':'115',
+                 'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.14) Gecko/20110221 Ubuntu/10.10 (maverick) Firefox/3.6.14'}
+
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+        urllib2.install_opener(opener) 
+        req = urllib2.Request(url)
+        opener.addheaders = heads.items()
+        respHtml = opener.open(req).read()
+    except Exception:
+        respHtml = ""
     return respHtml
 
 def getResultFromVirusTotal(html,url, file_hash,file_path):  
-    soup = BeautifulSoup(html)
-    html_doc=soup.find_all('tr')
-    #if html_doc is None:
-    #    print 'Error: Nothing can be grapped!'
-    #else:
-    #    print 'Done: News info was grapped with success!'
     result=""
     try:
+        soup = BeautifulSoup(html)
+        html_doc=soup.find_all('tr')
         ratio=html_doc[2].find_all('td')[1].find_all(text=True)[0].strip()
         result=file_hash+","+file_path+","+ratio+","+url+"\n"
         print file_hash+","+ratio
@@ -105,15 +107,33 @@ def virustotal(file_hash, file_path):
 def main():
     f = open('result_VT.txt','w') # Open log file
     # set up file path to scan on VirusTotal
-    path = r'C:\Users\123\Desktop\alertMon'
+    path = r'C:\Users\renfei\Desktop\EasyShip5.3.14_20140807_installation'
     file_hash = get_file_hash(path)
-    #print file_hash
+    print "-----------File hash calculation is done-----------"
     for eachhash in file_hash:
         file_hash = eachhash.split(',')[0]
         file_path = eachhash.split(',')[1]
         result = virustotal(file_hash,file_path)
         f.write(result)
     f.close()
+   
+
+def test(): 
+    f = open('result_VT.txt','w') # Open log file
+    # set up file path to scan on VirusTotal
+    path = r'C:\Users\renfei\Desktop\alertMon'
+    file_hash = get_file_hash(path)
+    #print file_hash
+    print "-----------File hash calculation is done-----------"
+    for eachhash in file_hash:
+        file_hash = eachhash.split(',')[0]
+        file_path = eachhash.split(',')[1]
+        result = virustotal(file_hash,file_path)
+        f.write(result)
+        #time.sleep(5)
+    f.close()
+    
     
 if __name__ == "__main__":
     main()
+    #test()
